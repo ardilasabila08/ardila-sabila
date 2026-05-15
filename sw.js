432eb6b5d9480ea52be439091e6d9ce48a0e93c1
ardilasabila08 <ardilasabila6@gmail.com>
2026-05-15 12:56:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,43 @@
-const CACHE_NAME = "cv-pwa-fix";
+const CACHE_NAME = "cv-ardila-final-v1";
 const urlsToCache = [
   "/",
   "/index.html",
+  "/manifest.json", // Ini harus ada!
   "/icon-192.png",
   "/icon-512.png",
   "/foto.jpeg"
 ];
 
+// Proses Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Caching assets...");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// Proses Aktivasi (Hapus cache lama biar fotonya update)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Proses Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
   );
 });
